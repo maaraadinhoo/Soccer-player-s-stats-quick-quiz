dda82145036a7ff1961cbdd2ec5a91fad571ef5b
maaraadinhoo <94734091+maaraadinhoo@users.noreply.github.com>
2022-08-22 23:07:07 -0400

    Add files via upload

---
+++ soccer.stats.quiz.py
@@ -0,0 +1,47 @@
+print('welcome to the quiz game press 1 and enter')
+print()
+score = 0
+enter = input()
+if enter == '1':
+  print('Welcome to the quiz game')
+  print("-"*50)
+  
+  
+else:
+  print('Bye bye')
+  quit()
+print()
+print("-"*50)
+print()
+answer = input('how many goals does C.Ronaldo have in his career? ')
+if answer == '814':
+  print('Bravo!')
+  score +=1
+
+else:
+  print('sorry wrong anwer')
+  score = 0
+print()
+print("-"*50)
+print()
+answer = input('how many goals does Neymar Jr have in his career? ')
+if answer == '420':
+  print('Bravo!')
+  score +=1
+  
+else:
+  print('sorry wrong anwer')
+  score = 0
+print()
+print("-"*50)
+print()
+answer = input('how many goals does L.Messi have in his career? ')
+if answer == '773':
+  print('Bravo!')
+  score +=1
+
+else:
+  print('sorry wrong anwer')
+  score = 0
+print('-'*50)
+print('you earned ' + str(score) + ' points today')
